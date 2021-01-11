0af8f63858ad3538f35912e2795097d2b17ad602
Huajian Mao <huajianmao@gmail.com>
2021-01-11 15:18:10 +0800

    Add url prefix for rest apis

--- src/main/java/cn/hjmao/auth/controller/TestController.java
+++ src/main/java/cn/hjmao/auth/controller/TestController.java
@@ -4,15 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("${api.prefix.v1}/tasks")
 public class TestController {
 
-  @RequestMapping("/tasks/hello")
+  @RequestMapping("/hello")
   public String get() {
     return "/hello world!";
   }
 
-  @RequestMapping("/")
+  @RequestMapping("")
   public String getRoot() {
-    return "/get root!";
+    return "/get task root!";
   }
 }
