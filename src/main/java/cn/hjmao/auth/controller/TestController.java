0a38187d5d1c5a00da44cdb703bb930013a11c0a
Huajian Mao <huajianmao@gmail.com>
2021-01-25 09:39:39 +0800

    Revision according to sonarqube report

--- src/main/java/cn/hjmao/auth/controller/TestController.java
+++ src/main/java/cn/hjmao/auth/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.hjmao.auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${api.prefix.v1}/tasks")
 public class TestController {
 
-  @RequestMapping("/hello")
+  @GetMapping("/hello")
   public String get() {
     return "/hello world!";
   }
 
-  @RequestMapping("")
+  @GetMapping("")
   public String getRoot() {
     return "/get task root!";
   }
---
+++ src/test/java/cn/hjmao/auth/config/JwtSettingsTest.java
@@ -0,0 +1,32 @@
+package cn.hjmao.auth.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JwtSettingsTest {
+
+  @Test
+  void newJwtSettingsShouldFail() {
+    try {
+      Constructor<JwtSettings> constructor = JwtSettings.class.getDeclaredConstructor();
+      assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+      constructor.setAccessible(true);
+      constructor.newInstance();
+    } catch (NoSuchMethodException e) {
+      assert false;
+    } catch (IllegalAccessException e) {
+      assert false;
+    } catch (InstantiationException e) {
+      assert false;
+    } catch (InvocationTargetException e) {
+      assert true;
+    }
+  }
+}
